3e6a6de8fc29152354122dbbb5a02eba7fbe0eb2
Al Aqmar <alaqmarak0810@gmail.com>
2025-01-31 11:47:32 +0530

    titles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,8 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Contact Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Contact Page for Startup Nextjs Template",
+  title: "Contact Page",
+  description: "Contact Page",
   // other metadata
 };
 
@@ -14,7 +14,7 @@ const ContactPage = () => {
     <>
       <Breadcrumb
         pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        description="Do you have an inquiry or just want to say hi? Feel free to contact us!"
       />
 
       <Contact />
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,12 @@
 import SingleBlog from '@/components/Blog/SingleBlog';
 import axios from 'axios'
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Products",
+  description: "Products",
+}
 
 const Products = async () => {
   const api = await axios.get("https://mystore.alaqmar.dev/api/stellar-industries/products")
@@ -10,15 +16,15 @@ const Products = async () => {
       className='container py-16 mt-16 md:py-20 lg:py-28'
     >
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
-          {
-            products.map((product, index) => (
-              <SingleBlog
-                key={index}
-                blog={product}
-              />
-            ))
-          }
-        </div>
+        {
+          products.map((product, index) => (
+            <SingleBlog
+              key={index}
+              blog={product}
+            />
+          ))
+        }
+      </div>
     </section>
   )
 }
