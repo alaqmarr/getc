c50a5f652d26d992c6ef4fac095e4b920661a364
alaqmarr <alaqmarak0810@gmail.com>
2025-09-13 14:22:00 +0530

    deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -121,12 +121,12 @@ const Header = () => {
                 <nav
                   id="navbarCollapse"
                   ref={navbarRef} // Attach the ref to the navbar
-                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
+                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 items-center ${navbarOpen
                     ? "visibility top-full opacity-100"
                     : "invisible top-[120%] opacity-0"
                     }`}
                 >
-                  <ul className="block lg:flex lg:space-x-12">
+                  <ul className="block lg:flex lg:space-x-12 items-center">
                     <li className="group relative">
                       <Link
                         href="/"
